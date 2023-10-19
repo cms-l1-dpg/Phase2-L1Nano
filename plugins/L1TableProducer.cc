7728e4237b4983f95f351458247b9611cdacba66
Artur Lobanov <Artur.Lobanov@cern.ch>
2023-10-19 16:07:07 +0200

    Fix Vtx table producer name

--- plugins/L1TableProducer.cc
+++ plugins/L1TableProducer.cc
@@ -1,7 +1,7 @@
 #include "PhysicsTools/NanoAOD/interface/SimpleFlatTableProducer.h"
 
 #include "DataFormats/L1Trigger/interface/VertexWord.h"
-typedef SimpleFlatTableProducer<l1t::VertexWord> SimpleVtxWordCandidateFlatTableProducer;
+typedef SimpleFlatTableProducer<l1t::VertexWord> SimpleL1VtxWordCandidateFlatTableProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(SimpleVtxWordCandidateFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleL1VtxWordCandidateFlatTableProducer);
