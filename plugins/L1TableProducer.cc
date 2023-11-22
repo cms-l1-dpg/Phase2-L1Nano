633c40629c3bf179af58042ed4c0876905f5a8f6
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2023-11-22 22:08:52 +0100

    Add SimpleL1TkJetWordFlatTableProducer

--- plugins/L1TableProducer.cc
+++ plugins/L1TableProducer.cc
@@ -3,5 +3,9 @@
 #include "DataFormats/L1Trigger/interface/VertexWord.h"
 typedef SimpleFlatTableProducer<l1t::VertexWord> SimpleL1VtxWordCandidateFlatTableProducer;
 
+#include "DataFormats/L1Trigger/interface/TkJetWord.h"
+typedef SimpleFlatTableProducer<l1t::TkJetWord> SimpleL1TkJetWordCandidateFlatTableProducer;
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(SimpleL1VtxWordCandidateFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleL1TkJetWordCandidateFlatTableProducer);
