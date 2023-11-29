c8e34dc365f39eac48cfebf56ca4801f64c2940b
Artur Lobanov <Artur.Lobanov@cern.ch>
2023-11-29 10:44:54 +0100

    Add P2GT AlgoBlock decision table

--- plugins/L1TableProducer.cc
+++ plugins/L1TableProducer.cc
@@ -3,5 +3,9 @@
 #include "DataFormats/L1Trigger/interface/VertexWord.h"
 typedef SimpleFlatTableProducer<l1t::VertexWord> SimpleL1VtxWordCandidateFlatTableProducer;
 
+#include "DataFormats/L1Trigger/interface/P2GTAlgoBlock.h"
+typedef SimpleFlatTableProducer<l1t::P2GTAlgoBlock> P2GTAlgoBlockFlatTableProducer;
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(SimpleL1VtxWordCandidateFlatTableProducer);
+DEFINE_FWK_MODULE(P2GTAlgoBlockFlatTableProducer);
